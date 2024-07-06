e5ca5cefd141f4d3dcb29887c98f60b2bda38420
DharaniKarunarathna <91901027+DharaniKarunarathna@users.noreply.github.com>
2024-07-06 08:42:37 +0000

    changes in button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 function App() {
   const name = 'StarGazers'
   return (
+    <>
     <div className="container">
       <article>
         <hgroup>
@@ -8,10 +9,11 @@ function App() {
           <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
+          <button className="Outline" onClick={()=> alert('Hi there')}>Click me</button> 
         </hgroup>
       </article>
     </div>
+    </>
   )
 }
 export default App
